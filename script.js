a4740015208b941c499a745dfce70724523ea428
Ryan Black <ryan.h.black@gmail.com>
2020-02-22 22:35:02 -0700

    NHL Franchises App

--- script.js
+++ script.js
@@ -1,158 +1,114 @@
-function loadCurrentWeatherByCity(city) {
-    const apiKey = "5a305dcb038e4a72653de0b774be24ad";
-    const url = "http://api.openweathermap.org/data/2.5/weather?q=" + city + ",US&units=imperial&appid=" + apiKey;
+function loadFranchiseList() {
+    const url = "https://records.nhl.com/site/api/franchise?include=teams.id&include=teams.active&include=teams.triCode&include=teams.placeName&include=teams.commonName&include=teams.fullName&include=teams.logos&include=teams.conference.name&include=teams.division.name&include=teams.franchiseTeam.firstSeason.id&include=teams.franchiseTeam.lastSeason.id&include=teams.franchiseTeam.teamCommonName";
     fetch(url)
         .then(function (response) {
             return response.json();
         }).then(function (json) {
-            let results = "";
-            results += '<h2>Current Weather in ' + json.name + '</h2><hr />';
-            results += '<div style="max-width: 500px; margin: auto;">';
-            for (let i = 0; i < json.weather.length; i++) {
-                results += '<img style="display: block; margin: auto;" src="http://openweathermap.org/img/w/' + json.weather[i].icon + '.png" alt="Weather Icon" />';
-            }
-            results += '<h2 style="text-align: center;">' + Math.round(json.main.temp) + ' &deg;F</h2>';
-            results += '<p style="text-align: center;"><strong>';
-            for (let i = 0; i < json.weather.length; i++) {
-                results += json.weather[i].description;
-                if (i !== json.weather.length - 1) {
-                    results == ", ";
+            let teamNames = [];
+            for (let i = 0; i < json.data.length; i++) {
+                let franchise = json.data[i];
+                let teamId = json.data[i].mostRecentTeamId;
+                let j = 0;
+                for (j = 0; j < json.data[i].teams.length; ++j) {
+                    if (franchise.teams[j].id == teamId) break;
                 }
+                teamNames.push([json.data[i].teams[j].fullName, json.data[i].id]);
             }
-            results += "</strong></p>";
-            results += '<div style="text-align: center;" class="col-xs-6">';
-            results += '<p><strong>Feels Like:</strong>';
-            results += ' ' + Math.round(json.main.feels_like) + '&deg;F</p>';
-            results += '<p><strong>Max/Min:</strong>';
-            results += ' ' + Math.round(json.main.temp_min) + '/';
-            results += Math.round(json.main.temp_max) + '&deg;F</p>';
-            results += '</div>';
-            results += '<div style="text-align: center;" class="col-xs-6">';
-            results += '<p><strong>Wind:</strong>';
-            results += ' ' + Math.round(json.wind.speed) + ' MPH @ ';
-            results += json.wind.deg + '&deg;</p>';
-            results += '<p><strong>Humidity:</strong>';
-            results += ' ' + json.main.humidity + '%</p>';
-            results += '</div>';
-            results += '</div>';
-            document.getElementById('current-weather').innerHTML = results;
-        });
-    return;
-}
-
-function loadForecastByCity(city) {
-    const apiKey = "5a305dcb038e4a72653de0b774be24ad";
-    const url = "http://api.openweathermap.org/data/2.5/forecast?q=" + city + ",US&units=imperial&appid=" + apiKey;
-    fetch(url)
-        .then(function (response) {
-            return response.json();
-        }).then(function (json) {
-            let results = "";
-            document.getElementById("forecast-header").innerHTML = 'Forecast for ' + json.city.name;
-            document.getElementById("3-day").classList.add("active");
-            document.getElementById("5-day").classList.remove("active");
-            results += '<table id="forecast-table">';
-            results += '<tr>';
-            for (let i = 0; i < 6; i += 1) {
-                var days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
-                var date = new Date(json.list[i].dt_txt);
-                results += '<td>';
-                results += '<p style="text-align: center;"><strong>';
-                results += days[date.getDay()];
-                results += ' ' + date.getDate() + ' ';
-                if (date.getHours() == 0) {
-                    results += '- Midnight';
-                } else if (date.getHours() < 12) {
-                    results += '- ' + date.getHours() + 'AM';
-                } else if (date.getHours() == 12) {
-                    results += '- Noon';
+            teamNames.sort();
+            let sortedTeamIds = [];
+            for (let i = 0; i < teamNames.length; i++) {
+                sortedTeamIds.push(teamNames[i][1]);
+            }
+            let table = "<table class=\"col-xs-12\"id=\"teams-table\">";
+            let col = 0;
+            for (let i = 0; i < sortedTeamIds.length; i++) {
+                if (col > 5) col = 0;
+                if (col == 0) table += "<tr>";
+                let franchise = json.data[sortedTeamIds[i] - 1];
+                let teamId = franchise.mostRecentTeamId;
+                let j = 0;
+                for (j = 0; j < franchise.teams.length; ++j) {
+                    if (franchise.teams[j].id == teamId) {
+                        break;
+                    }
+                }
+                let team = franchise.teams[j];
+                table += "<td><img src=\"";
+                if (team.logos[0].background == "light") {
+                    table += team.logos[0].url;
                 } else {
-                    results += '- ' + (date.getHours() - 12) + 'PM';
+                    table += team.logos[1].url;
                 }
-                results += '</strong></p>'
-                results += '<img style="display: block; margin: auto;" src="http://openweathermap.org/img/w/' + json.list[i].weather[0].icon + '.png" alt="Weather Icon" />';
-                results += '<p style="text-align: center;"><strong>';
-                results += Math.round(json.list[i].main.temp);
-                results += '&deg;F</strong><br />'
-                results += Math.round(json.list[i].main.temp_min) + '/';
-                results += Math.round(json.list[i].main.temp_max);
-                results += '&deg;F</p>'
-                results += '</td>';
+                table += "\" alt=\"";
+                table += team.fullName + " logo";
+                table += "\" title=\"";
+                table += team.fullName;
+                table += "\" onclick=\"";
+                table += "loadFranchiseByID(" + json.data[sortedTeamIds[i] - 1].mostRecentTeamId;
+                table += ", '" + team.fullName;
+                table += "');\" /></td>";
+                if (col == 5) table += "</tr>";
+                col++;
             }
-            results += '</tr>';
-            results += '</table>'
-            document.getElementById('forecast').innerHTML = results;
+            document.getElementById("records").innerHTML = table;
+            document.getElementById("franchise").innerHTML = "NHL Franchises"
+            console.log(json);
         });
     return;
 }
 
-function load5DayForecastByCity(city) {
-    const apiKey = "5a305dcb038e4a72653de0b774be24ad";
-    const url = "http://api.openweathermap.org/data/2.5/forecast?q=" + city + ",US&units=imperial&appid=" + apiKey;
+function loadFranchiseByID(id = 1, teamName = "") {
+    const url = "https://records.nhl.com/site/api/franchise-detail?cayenneExp=mostRecentTeamId=" + id;
+
     fetch(url)
         .then(function (response) {
             return response.json();
         }).then(function (json) {
-            document.getElementById("3-day").classList.remove("active");
-            document.getElementById("5-day").classList.add("active");
-            let results = "";
-            results += '<table id="forecast-table">';
-            results += '<tr>';
-            for (let i = 0; i < 40; i += 8) {
-                var days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
-                var date = new Date(json.list[i].dt_txt);
-                results += '<td>';
-                results += '<p style="text-align: center;"><strong>';
-                results += days[date.getDay()];
-                results += ' ' + date.getDate() + '</strong></p>'
-                results += '<img style="display: block; margin: auto;" src="http://openweathermap.org/img/w/' + json.list[i].weather[0].icon + '.png" alt="Weather Icon" />';
-                results += '<p style="text-align: center;"><strong>';
-                results += Math.round(json.list[i].main.temp);
-                results += '&deg;F</strong><br />'
-                results += Math.round(json.list[i].main.temp_min) + '/';
-                results += Math.round(json.list[i].main.temp_max);
-                results += '&deg;F</p>'
-                results += '</td>';
+            let info = "<p style=\"cursor: pointer;\"onclick=\"loadFranchiseList();\"><span class=\"glyphicon glyphicon-arrow-left\"></span> Return to Franchise List</p>"
+            let team = json.data[0]
+            info += "<img class=\"hero-image\" alt=\"hero-image\" src=\"";
+            info += team.heroImageUrl + "\" />";
+            info += "<div style=\"text-align: center;\" class=\"col-xs-12\">";
+            info += "<h3>Team Information</h3><hr />";
+            let creationDate = new Date(team.dateAwarded);
+            const month = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+            let dateString = month[creationDate.getMonth()] + " " + creationDate.getMonth() + ", " + creationDate.getFullYear();
+            info += "<p><strong>Creation Date:</strong> " + dateString + "</p>"
+            info += "<p><strong>First Season:</strong> " + Math.floor(team.firstSeasonId / 10000) + "-" + Math.ceil(team.firstSeasonId / 10000) + "</p>";
+            if (team.directoryUrl != null) {
+                info += "<p><strong>Staff Directory:</strong> <a href=\"" + team.directoryUrl + "\">" + team.directoryUrl + "</a></p>"
+                info += "</div>";
+                info += "<hr style=\"clear: both;\" />";
+                info += "<div class=\"col-xs-12 col-md-6\">";
+                info += "<h3>Captains</h3><hr />";
+                info += team.captainHistory;
+                info += "</div>";
+                info += "<div class=\"col-xs-12 col-md-6\">";
+                info += "<h3>Coaches</h3><hr />";
+                info += team.coachingHistory;
+                info += "</div>";
+                info += "<hr style=\"clear: both;\" />";
+                info += "<div class=\"col-xs-12 col-md-6\">";
+                info += "<h3>Retired Numbers</h3><hr />";
+                info += team.retiredNumbersSummary;
+                info += "</div>";
+                info += "<div class=\"col-xs-12 col-md-6\">";
+                info += "<h3>General Managers</h3><hr />";
+                info += team.generalManagerHistory;
+                info += "</div>";
+            } else {
+                info += "<p>There is no additional data available for the " + teamName + " because they are no longer the the NHL</p>";
+                info += "</div>";
             }
-            results += '</tr>';
-            results += '</table>'
-            document.getElementById('forecast').innerHTML = results;
+            info += "<hr style=\"clear: both;\" /><p style=\"cursor: pointer;\"onclick=\"loadFranchiseList();\"><span class=\"glyphicon glyphicon-arrow-left\"></span> Return to Franchise List</p>"
+            document.getElementById("records").innerHTML = info;
+            document.getElementById("franchise").innerHTML = teamName;
+            console.log(team);
         });
     return;
 }
-
-document.getElementById("5-day").addEventListener("click", function (event) {
-    event.preventDefault();
-    if (document.getElementById("currentCity").value) {
-        load5DayForecastByCity(document.getElementById("currentCity").value)
-    } else {
-        load5DayForecastByCity("Provo");
-    }
-    return;
-});
-
-document.getElementById("3-day").addEventListener("click", function (event) {
-    event.preventDefault();
-    if (document.getElementById("currentCity").value) {
-        loadForecastByCity(document.getElementById("currentCity").value)
-    } else {
-        loadForecastByCity("Provo");
-    }
-    return;
-});
-
-document.getElementById("weatherSubmit").addEventListener("click", function (event) {
-    event.preventDefault();
-    if (document.getElementById("weatherInput").value) {
-        loadCurrentWeatherByCity(document.getElementById("weatherInput").value);
-        loadForecastByCity(document.getElementById("weatherInput").value);
-        document.getElementById("currentCity").value = document.getElementById("weatherInput").value;
-    }
-    return;
-})
 
 window.addEventListener("load", function (event) {
     event.preventDefault();
-    loadCurrentWeatherByCity("Provo");
-    loadForecastByCity("Provo");
+    loadFranchiseList();
 });
